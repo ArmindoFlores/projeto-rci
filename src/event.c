5e873bba0b516ba76b3146afd4ee53ac9078aa27
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-03-15 10:38:17 +0000

    Restructuring

---
+++ src/event.c
@@ -0,0 +1,68 @@
+#include "event.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+t_event select_event(t_nodeinfo* ni)
+{
+    // If there's pending reads in any of the connections, do them first
+    if (ni->nextfd > 0 && has_available_data(ni->successor))
+        return E_MESSAGE_SUCCESSOR;
+    if (ni->prevfd > 0 && has_available_data(ni->predecessor))
+        return E_MESSAGE_PREDECESSOR;
+    if (ni->tempfd > 0 && has_available_data(ni->temp))
+        return E_MESSAGE_TEMP;
+
+    // Initialize file descriptor set
+    fd_set read_fds;
+    FD_ZERO(&read_fds); 
+    int fdmax = maxfd(ni);
+    fdmax = fdmax > STDIN_FILENO ? fdmax : STDIN_FILENO;
+
+    // Add currently in-use file descriptors to the set
+    FD_SET(STDIN_FILENO, &read_fds);
+    if (ni->mainfd > 0)
+        FD_SET(ni->mainfd, &read_fds);
+    if (ni->nextfd > 0)
+        FD_SET(ni->nextfd, &read_fds);
+    if (ni->prevfd > 0)
+        FD_SET(ni->prevfd, &read_fds);
+    if (ni->tempfd > 0)
+        FD_SET(ni->tempfd, &read_fds);
+
+    int count = select(fdmax+1, &read_fds, NULL, NULL, NULL);
+    if (count < 0) {
+        puts("\x1b[31mSelect error!\033[m");
+        exit(1);
+    }
+    while (count--) {
+        // Check for ready fd's with FD_ISSET
+        if (FD_ISSET(ni->mainfd, &read_fds)) {
+            // Incoming connection
+            FD_CLR(ni->mainfd, &read_fds);
+            return E_INCOMING_CONNECTION;
+        }
+        else if (FD_ISSET(ni->nextfd, &read_fds)) {
+            // Incoming message from successor
+            FD_CLR(ni->nextfd, &read_fds);
+            return E_MESSAGE_SUCCESSOR;
+        }
+        else if (FD_ISSET(ni->prevfd, &read_fds)) {
+            // Incoming message from predecessor
+            FD_CLR(ni->prevfd, &read_fds);
+            return E_MESSAGE_PREDECESSOR;
+        }
+        else if (FD_ISSET(ni->tempfd, &read_fds)) {
+            // Incoming message from somewhere else
+            FD_CLR(ni->tempfd, &read_fds);
+            return E_MESSAGE_TEMP;
+        }
+        else if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            FD_CLR(STDIN_FILENO, &read_fds);
+            return E_MESSAGE_USER;
+        }
+    }
+
+    return E_ERROR;
+}
---
+++ src/event.h
@@ -0,0 +1,23 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "common.h"
+
+typedef enum event {
+    E_INCOMING_CONNECTION,
+    E_MESSAGE_PREDECESSOR,
+    E_MESSAGE_SUCCESSOR,
+    E_MESSAGE_TEMP,
+    E_MESSAGE_USER,
+    E_ERROR
+} t_event;
+
+/**
+ * @brief Blocks until an event occurrs, then returns it
+ * 
+ * @param ni necessary information about the node 
+ * @return [ @b t_event ] what event has occurred
+ */
+t_event select_event(t_nodeinfo *ni);
+
+#endif
